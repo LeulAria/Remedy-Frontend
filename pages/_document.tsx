ca33e684d51fd43cbed9c3cacc695cd1caca3349
LeulAria <leulsegedtariku@gmail.com>
2021-11-21 22:46:34 +0300

    fixon typescipt deploy build error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
@@ -16,7 +16,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700;900&display=swap"
             rel="stylesheet"
